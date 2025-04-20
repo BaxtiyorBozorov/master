c92b13e7d7329fab6d897945722415055631a35f
BaxtiyorBozorov <baxtiyorbozorov226@gmail.com>
2025-04-20 23:36:44 +0500

    add migration and master premium column added

--- src/database/migrations/v1/20250420112540_update_masters.ts
+++ src/database/migrations/v1/20250420112540_update_masters.ts
@@ -1,20 +1,15 @@
-import type { Knex } from "knex";
-
+import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.alterTable('masters', table => {
-    table.dropColumn('category_id');
-    table.string('category_id').references('categories.id').onDelete('CASCADE');
-  }
-  );
-  await knex.schema.alterTable('users', table => {  
-    table.dropColumn('role');
-    table.string('role').references('roles.id').onDelete('CASCADE');
-  }
-  );
+    await knex.schema.alterTable('masters', (table) => {
+        table.boolean('is_premium').defaultTo(false);
+        table.date('premium_until').defaultTo(null);
+    });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+    await knex.schema.alterTable('masters', (table) => {
+        table.dropColumn('is_premium');
+        table.dropColumn('premium_until');
+    });
 }
-
